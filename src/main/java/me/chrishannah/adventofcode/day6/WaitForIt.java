018d28c172d9b43e668c85c4893ff0ac050542e9
Christopher Hannah <me@chrishannah.me>
2023-12-06 14:15:08 -0100

    Improved Day 6

--- src/main/java/me/chrishannah/adventofcode/day6/WaitForIt.java
+++ src/main/java/me/chrishannah/adventofcode/day6/WaitForIt.java
@@ -5,48 +5,45 @@ import me.chrishannah.adventofcode.AOCUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 public class WaitForIt {
 
-    private static int calculateNumberOfPossibleWinningSolutions(int time, int recordDistance) {
-        var winningSolutions = 0;
-        for (int i = 1; i < time; i++) {
-            var distance = (time - i) * i;
-            if (distance > recordDistance) {
-                winningSolutions++;
-            }
-        }
-        return winningSolutions;
-    }
-
-    private static int calculateNumberOfPossibleWinningSolutions(long time, long recordDistance) {
-        var winningSolutions = 0;
+    private static long calculateNumberOfPossibleWinningSolutions(long time, long recordDistance) {
+        var firstWinningTime = 0L;
+        var lastWinningTime = 0L;
         for (long i = 1; i < time; i++) {
-            var distance = (time - i) * i;
-            if (distance > recordDistance) {
-                winningSolutions++;
+            if ((time - i) * i > recordDistance) {
+                firstWinningTime = i;
+                break;
             }
         }
-        return winningSolutions;
+        for (long i = time; i > 0; i--) {
+            if ((time - i) * i > recordDistance) {
+                lastWinningTime = i;
+                break;
+            }
+        }
+        return lastWinningTime - firstWinningTime;
     }
 
-    private static int calculateProductOfTotalWinningSolutions() {
+    private static long calculateProductOfTotalWinningSolutions() {
         var reader = AOCUtils.readFile("src/main/resources/day-6-input.txt");
-        var solutions = new ArrayList<Integer>();
+        var solutions = new ArrayList<Long>();
         try {
             var times = Arrays.stream(reader.readLine().substring(5).split(" ")).filter(s -> !s.isEmpty()).toList();
             var distances = Arrays.stream(reader.readLine().substring(9).split(" ")).filter(s -> !s.isEmpty()).toList();
             for (int i = 0; i < times.size(); i++) {
-                var t = Integer.parseInt(times.get(i));
-                var d = Integer.parseInt(distances.get(i));
+                var t = Long.parseLong(times.get(i));
+                var d = Long.parseLong(distances.get(i));
                 solutions.add(calculateNumberOfPossibleWinningSolutions(t, d));
             }
         } catch (IOException ignored) {
         }
-        return solutions.stream().reduce((a, b) -> a * b).orElse(0);
+        return solutions.stream().reduce((a, b) -> a * b).orElse(0L);
     }
 
-    private static int calculateWinningSolutionToSingleRace() {
+    private static long calculateWinningSolutionToSingleRace() {
         var reader = AOCUtils.readFile("src/main/resources/day-6-input.txt");
         try {
             var time = Long.parseLong(reader.readLine().substring(5).replace(" ", ""));
@@ -58,11 +55,16 @@ public class WaitForIt {
     }
 
     public static void main(String[] args) {
-
         // Part 1
-        System.out.println(calculateProductOfTotalWinningSolutions());
+        var start = new Date().getTime();
+        var answer = calculateProductOfTotalWinningSolutions();
+        var stop = new Date().getTime();
+        System.out.println(STR. "Part 1 | Answer: \{ answer } Time: \{ stop - start }ms" );
 
         // Part 2
-        System.out.println(calculateWinningSolutionToSingleRace());
+        start = new Date().getTime();
+        answer = calculateWinningSolutionToSingleRace();
+        stop = new Date().getTime();
+        System.out.println(STR. "Part 2 | Answer: \{ answer } Time: \{ stop - start }ms" );
     }
 }
